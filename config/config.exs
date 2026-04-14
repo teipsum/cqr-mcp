6be2140a38a3a723183dd79bebdd8577247ad022
Michael Lewis Cram <mlcram@teipsum.com>
2026-04-14 11:43:07 -0400

    feat: configurable certification preservation policy for UPDATE (closes #22)

--- config/config.exs
+++ config/config.exs
@@ -2,4 +2,6 @@ import Config
 
 config :logger, :default_handler, config: [type: :standard_error]
 
+config :cqr_mcp, certification_preservation_policy: :standard
+
 import_config "#{config_env()}.exs"
